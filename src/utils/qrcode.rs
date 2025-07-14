500284b14b5eae8f47312ac9997dc7cd5433a244
Juerson <37030166+juerson@users.noreply.github.com>
2025-07-14 10:47:12 +0800

    base64版本升级，导致代码写法也要改

--- src/utils/qrcode.rs
+++ src/utils/qrcode.rs
@@ -1,7 +1,8 @@
-use base64::encode;
-use image::{ ImageFormat, Luma };
+use base64::{engine::general_purpose, Engine};
+use image::{ImageFormat, Luma};
 use qrcode::QrCode;
 use std::io::Cursor;
+
 /// 生成包含二维码和内容的 HTML 页面
 pub fn generate_html_with_qrcode(content: &str, url: &str) -> String {
     let code = QrCode::new(url).unwrap();
@@ -9,7 +10,7 @@ pub fn generate_html_with_qrcode(content: &str, url: &str) -> String {
     let image = code.render::<Luma<u8>>().build();
     let mut buffer = Cursor::new(Vec::new());
     image.write_to(&mut buffer, ImageFormat::Png).unwrap();
-    let base64_qrcode = encode(buffer.get_ref());
+    let base64_qrcode = general_purpose::STANDARD.encode(buffer.get_ref());
 
     // 构建HTML内容
     format!(
@@ -19,7 +20,6 @@ pub fn generate_html_with_qrcode(content: &str, url: &str) -> String {
         <p>扫描以下二维码以便在手机上查看：</p>
         <img src="data:image/png;base64,{}" />
         "#,
-        content,
-        base64_qrcode
+        content, base64_qrcode
     )
 }
