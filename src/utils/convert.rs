1cc74225af849f922c0927dc7585da03f9cdf1d2
Juerson <37030166+juerson@users.noreply.github.com>
2025-07-14 10:46:11 +0800

    改为以tls字段值主，是否跳过选择其它的配置

--- src/utils/convert.rs
+++ src/utils/convert.rs
@@ -1,5 +1,6 @@
-use super::{clash, config::get_yaml_value, singbox, v2ray};
-use rand::{seq::SliceRandom, Rng};
+use super::config::{get_yaml_value, get_yaml_value_with_fallback};
+use super::{clash, singbox, v2ray};
+use rand::prelude::IndexedRandom;
 use serde_yaml::Value as YamlValue;
 
 pub fn subconvert(
@@ -11,6 +12,7 @@ pub fn subconvert(
     uri_proxy_type: String,
     uri_tls_mode: String,
     uri_userid: u8,
+    skip_transport: bool,
     http_ports: &[u16; 7],
     https_ports: &[u16; 6],
 ) -> (String, String) {
@@ -23,124 +25,125 @@ pub fn subconvert(
         false => format!("{} | ", csv_alias),
     };
     if let Some(sequence) = yamlvalue.clone().as_sequence_mut() {
-        let length = sequence.len();
-        let mut index;
-
         // 循环200次，直到选中合适的节点配置为止，或循环200次才跳出循环
         for _ in 0..200 {
-            // 使用config.yaml中具体哪个节点的配置
-            index = match (1..=(length + 1) as u8).contains(&uri_userid) {
-                true => (uri_userid as usize) - 1, // 选择指定的（数组的下标）
-                false => rand::thread_rng().gen_range(0..length), // 随机选择（数组的下标）
-            };
+            let choose_item = sequence.choose(&mut rand::rng()).unwrap();
+            let mut choose_item_clone = choose_item.clone();
 
-            let random_https_port = https_ports.choose(&mut rand::thread_rng()).unwrap_or(&443);
-            let random_http_port = http_ports.choose(&mut rand::thread_rng()).unwrap_or(&8080);
+            // ************** 1、初步排除不要的配置（对比配置的id和type） **************
 
-            if let Some(yaml_value) = sequence.get_mut(index) {
-                let mut yaml_value_clone = yaml_value.clone();
+            let id = get_yaml_value(&choose_item_clone, &"id")
+                .and_then(|v| v.as_str().and_then(|s| s.parse::<usize>().ok()))
+                .unwrap_or(0);
 
-                let node_type = yaml_value_clone
-                    .get("type")
-                    .and_then(|v| v.as_str())
-                    .map(|s| s.to_string())
-                    .unwrap_or_else(String::new);
-
-                let host = match node_type.as_str() {
-                    "ss" => get_yaml_value(&yaml_value_clone, &["plugin-opts", "host"])
-                        .and_then(|v| v.as_str())
-                        .unwrap_or_default(),
-                    "vless" | "trojan" => {
-                        get_yaml_value(&yaml_value_clone, &["ws-opts", "headers", "Host"])
-                            .and_then(|v| v.as_str())
-                            .unwrap_or_default()
-                    }
-                    _ => {
-                        continue;
-                    }
-                };
-
-                // 处理 port 、 workers.dev、tls 之间与端口的问题，端口不对就随机生成一个
-                let is_workers_dev = host.ends_with("workers.dev");
-                if node_type == "ss" {
-                    let tls_val = get_yaml_value(&yaml_value_clone, &["plugin-opts", "tls"])
-                        .and_then(|v| v.as_bool())
-                        .unwrap_or(true);
-                    if tls_val && (port == 0 || is_http_ports(port)) {
-                        port = *random_https_port;
-                    } else if !tls_val && (port == 0 || is_https_ports(port)) {
-                        port = *random_http_port;
-                    }
-                } else if ["vless", "trojan"].contains(&node_type.as_str()) {
-                    if is_workers_dev && (port == 0 || is_https_ports(port)) {
-                        port = *random_http_port;
-                    } else if !is_workers_dev && (port == 0 || is_http_ports(port)) {
-                        port = *random_https_port;
-                    }
-                }
-
-                // 根据uri的tls筛选节点配置，是否使用这个节点生成订阅
-                if (is_http_ports(port) && uri_tls_mode == "true")
-                    || (is_https_ports(port) && uri_tls_mode == "false")
-                {
-                    continue;
-                }
-
-                if uri_proxy_type == node_type || uri_proxy_type == "all" {
-                    // 节点序号/账号的序号(从1开始)
-                    let padded_index =
-                        format!("{:0width$}", index + 1, width = length.to_string().len());
-                    // 构建完整的节点名称
-                    let remarks: String = format!(
-                        "【{}】{}{}:{}",
-                        padded_index,
-                        csv_remarks,
-                        csv_addr.clone(),
-                        port
-                    );
-                    match uri_target.as_str() {
-                        "v2ray" => {
-                            let (remarks_name, link) = v2ray::build_v2ray_links(
-                                &node_type,
-                                &mut yaml_value_clone,
-                                remarks,
-                                csv_addr,
-                                port,
-                            );
-                            if !remarks_name.is_empty() {
-                                return (remarks_name, link);
-                            }
-                        }
-                        "clash" => {
-                            let clash_node = clash::build_clash_yaml(
-                                &mut yaml_value_clone,
-                                remarks.clone(),
-                                csv_addr,
-                                port,
-                            );
-                            let json_node: String = serde_json::to_string(&clash_node).unwrap();
-                            let json_string = format!("  - {json_node}");
-                            return (remarks, json_string);
-                        }
-                        "singbox" => {
-                            let (remarks_name, json_string) = singbox::build_singbox_config_json(
-                                &node_type,
-                                &mut yaml_value_clone,
-                                remarks,
-                                csv_addr,
-                                port,
-                            );
-                            if !remarks_name.is_empty() {
-                                return (remarks_name, json_string);
-                            }
-                        }
-
-                        _ => {}
-                    }
-
-                    break;
-                }
+            let node_type = choose_item
+                .get("type")
+                .and_then(|v| v.as_str())
+                .map(|s| s.to_string())
+                .unwrap_or("all".to_string());
+            let userid = uri_userid as usize;
+            if (uri_proxy_type != "all" && uri_proxy_type != node_type)
+                || (userid != 0 && userid != id)
+            {
+                continue;
             }
+
+            // ******************** 2、再次排除tls模式不符合的配置 ********************
+
+            // 只有trojan根据sni或servername字段的值来判断tls，其它按照tls值判断
+            let node_tls: String = match node_type.as_str() {
+                "vless" | "vmess" | "ss" => {
+                    get_yaml_value_with_fallback(choose_item, &["tls", "plugin-opts.tls"])
+                        .map(|s| s.to_string())
+                        .or_else(|| {
+                            ["tls", "plugin-opts.tls"]
+                                .iter()
+                                .filter_map(|&path| get_yaml_value(choose_item, path))
+                                .find_map(|v| v.as_bool().map(|b| b.to_string()))
+                        })
+                        .unwrap_or("false".to_string())
+                }
+                "trojan" => {
+                    let servername =
+                        get_yaml_value_with_fallback(choose_item, &["sni", "servername"])
+                            .unwrap_or_default();
+                    (!servername.ends_with("workers.dev")).to_string()
+                }
+                _ => "true".to_string(), // 不支持的协议，默认是true，可以使用clash
+            };
+            if (uri_tls_mode != "all" && uri_tls_mode != node_tls)
+                || (node_tls == "true" && is_http_ports(port))
+                || (node_tls == "false" && is_https_ports(port))
+            {
+                continue;
+            }
+
+            // ************************ 端口不存在的，随机端口 ************************
+
+            let random_https_port = https_ports.choose(&mut rand::rng()).unwrap_or(&443);
+            let random_http_port = http_ports.choose(&mut rand::rng()).unwrap_or(&8080);
+            if node_tls == "true" && port == 0 {
+                port = *random_https_port;
+            } else if node_tls == "false" && port == 0 {
+                port = *random_http_port;
+            }
+
+            let mut remarks = format!("【{}】{}{}:{}", id, csv_remarks, csv_addr.clone(), port);
+            match uri_target.as_str() {
+                "v2ray" => {
+                    // 是否将ss节点添加到v2rayN中使用，需要就只写别名、地址、端口、密码和加密方式，其它需要自己手动补充
+                    let mut skip_ss_transport = false;
+                    if uri_proxy_type == "ss"
+                        && node_type == "ss"
+                        && node_tls == "true"
+                        && skip_transport
+                    {
+                        let host = get_yaml_value(&choose_item, &"plugin-opts.host")
+                            .and_then(|v| v.as_str())
+                            .unwrap_or("");
+                        remarks = format!("❓{}", host);
+                        skip_ss_transport = true;
+                    }
+                    let (remarks_name, link) = v2ray::build_v2ray_links(
+                        &node_type,
+                        &mut choose_item_clone,
+                        remarks,
+                        csv_addr,
+                        port,
+                        skip_ss_transport,
+                    );
+                    if !remarks_name.is_empty() {
+                        return (remarks_name, link);
+                    }
+                }
+                "singbox" => {
+                    let (remarks_name, json_string) = singbox::build_singbox_config_json(
+                        &node_type,
+                        &mut choose_item_clone,
+                        remarks,
+                        csv_addr,
+                        port,
+                    );
+                    if !remarks_name.is_empty() {
+                        return (remarks_name, json_string);
+                    }
+                }
+                "clash" => {
+                    let clash_node = clash::build_clash_yaml(
+                        &mut choose_item_clone,
+                        remarks.clone(),
+                        csv_addr,
+                        port,
+                    );
+                    let json_node: String = serde_json::to_string(&clash_node).unwrap();
+                    let json_string = format!("  - {json_node}");
+                    return (remarks, json_string);
+                }
+
+                _ => {}
+            }
+
+            break;
         }
     }
 
